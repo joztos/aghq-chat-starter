9c7cdedda9447e5d4f01eb7fd5b305310277c3c9
josuetoz <johnskywalkr@gmail.com>
2023-03-12 20:02:31 -0600

    vercel adapter plugin

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import mix from "vite-plugin-mix";
+import mix, { vercelAdapter } from 'vite-plugin-mix'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     mix({
       handler: "./src/api.js",
+      adapter: vercelAdapter(),
+
     }),
   ],
   resolve: {
